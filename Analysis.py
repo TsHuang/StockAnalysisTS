a3dc647cc136db6abb51c23a31b32b5074c15f7b
TsHuang <tshuang1983@gmail.com>
2018-04-04 10:18:30 +0800

    add time series crawler

--- Analysis.py
+++ Analysis.py
@@ -1,6 +1,10 @@
 from crawler import *
+import datetime
+import time
+import matplotlib.pyplot as plt
 
-def pick_company(df, companys):
+
+def pick_company(df, companys):  # need to finish this later
     return df.loc[companys]
 
 def pick_stock(df):
@@ -9,6 +13,61 @@ def pick_stock(df):
     return df[cond1 & cond2]
 
 
+def nday_price(n_days):
+    data = {}
+    # n_days = 9
+    date = datetime.datetime.now()
+    fail_count = 0
+    allow_continuous_fail_count = 5
+    while len(data) < n_days:
+        print('parsing', date)
+        # 使用 crawPrice 爬資料
+        try:
+            # 抓資料
+            data[date] = crawl_price(date)
+            print('success!')
+            fail_count = 0
+        except:
+            # 假日爬不到
+            print('fail! check the date is holiday')
+            fail_count += 1
+            if fail_count == allow_continuous_fail_count:
+                raise
+                break
+
+        # 減一天
+        date -= datetime.timedelta(days=1)
+        time.sleep(10)
+
+    open = pd.DataFrame({k: d['開盤價'] for k, d in data.items()}).transpose()
+    open.index = pd.to_datetime(open.index)
+
+    close = pd.DataFrame({k: d['收盤價'] for k, d in data.items()}).transpose()
+    close.index = pd.to_datetime(close.index)
+
+    high = pd.DataFrame({k: d['最高價'] for k, d in data.items()}).transpose()
+    high.index = pd.to_datetime(high.index)
+
+    low = pd.DataFrame({k: d['最低價'] for k, d in data.items()}).transpose()
+    low.index = pd.to_datetime(low.index)
+
+    volume = pd.DataFrame({k: d['成交股數'] for k, d in data.items()}).transpose()
+    volume.index = pd.to_datetime(volume.index)
+
+    tsmc = {
+        'close': close['2330']['2017'].dropna().astype(float),
+        'open': open['2330']['2017'].dropna().astype(float),
+        'high': high['2330']['2017'].dropna().astype(float),
+        'low': low['2330']['2017'].dropna().astype(float),
+        'volume': volume['2330']['2017'].dropna().astype(float),
+    }
+
+    return tsmc
+
 if __name__ == "__main__":
-    df = financial_statement(2017, 2, '營益分析彙總表')
-    print(pick_stock(df))
+    # pick stock example
+    # df = financial_statement(2017, 2, '營益分析彙總表')
+    # print(pick_stock(df))
+
+    out = nday_price(9)
+    out['close'].plot()
--- crawler.py
+++ crawler.py
@@ -3,8 +3,20 @@ from io import StringIO
 import pandas as pd
 import numpy as np
 import time
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
+
+def crawl_price(date):
+    r = requests.post(
+        'http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + str(date).split(' ')[0].replace('-',
+                                                                                                              '') + '&type=ALL')
+    ret = pd.read_csv(StringIO("\n".join([i.translate({ord(c): None for c in ' '})
+                                          for i in r.text.split('\n')
+                                          if len(i.split('",')) == 17 and i[0] != '='])), header=0)
+    ret = ret.set_index('證券代號')
+    ret['成交金額'] = ret['成交金額'].str.replace(',', '')
+    ret['成交股數'] = ret['成交股數'].str.replace(',', '')
+    return ret
 
 def daily_report(datestr):
     r = requests.post('http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + datestr + '&type=ALL')
