99f95fc2cf6f635c06678f1f48fa79e9b0ecbe4a
TsHuang <tshuang1983@gmail.com>
2018-05-09 08:35:57 +0800

    Update StockAyGUI add several simple component to learn pyQT

--- StockAyGUI.py
+++ StockAyGUI.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QFormLayout
 
 class MainWindow(QWidget):
 
@@ -18,7 +18,27 @@ class MainWindow(QWidget):
         self.setWindowTitle("Hello World!")
 
 
+        self.label = QLabel()
+        self.label.setText("Cheers!")
 
+        self.button_hello = QPushButton()
+        self.button_hello.setText("hello")
+
+        self.button_world = QPushButton()
+        self.button_world.setText("world")
+
+        self.line_hello = QLineEdit()
+        self.line_world = QLineEdit()
+
+        form_layout = QFormLayout()
+        form_layout.addRow(self.button_hello, self.line_hello)
+        form_layout.addRow(self.button_world, self.line_world)
+
+        h_layout = QVBoxLayout()
+        h_layout.addWidget(self.label)
+        h_layout.addLayout(form_layout)
+
+        self.setLayout(h_layout)
 
 
 if __name__ == "__main__":
