632e6ac43a75b17f958303c1b1c2e84cae7701a9
TsHuang <tshuang1983@gmail.com>
2018-05-12 23:52:45 +0800

    Update plot

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 
@@ -7,6 +8,7 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
+    plotly.tools.set_credentials_file(username='tshuang1983', api_key='SLFEuRSvKP6v7sQkB69B')
     df = web.DataReader("aapl", 'morningstar').reset_index()
 
     trace = go.Candlestick(x=df.Date,
