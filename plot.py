55ec9a2e9608e0120d21b4e8350e6503b17565f7
TsHuang <tshuang1983@gmail.com>
2018-05-12 16:03:27 +0800

    Update plot

--- plot.py
+++ plot.py
@@ -1,6 +1,23 @@
 import matplotlib.pyplot as plt
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import pandas_datareader as web
+from datetime import datetime
+
 
 if __name__ == "__main__":
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
-    plt.show()
+    df = web.DataReader("aapl", 'morningstar').reset_index()
+
+    trace = go.Candlestick(x=df.Date,
+                           open=df.Open,
+                           high=df.High,
+                           low=df.Low,
+                           close=df.Close)
+    data = [trace]
+    print(data)
+    py.plot(data, filename='simple_candlestick')
+    # print(plotly.__version__)
+    # plt.plot([1, 2, 3, 4])
+    # plt.ylabel('some numbers')
+    # plt.show()
