37219dbc368ce3cef3030ed6a321990df739edf9
TsHuang <tshuang1983@gmail.com>
2018-05-20 22:34:25 +0800

    Add functions to plot the candlestick plot

--- plot.py
+++ plot.py
@@ -3,45 +3,29 @@ import pandas as pd
 import sqlite3
 import numpy as np
 
+# for candlestick plot
 import matplotlib.pyplot as plt
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+from matplotlib.finance import candlestick_ohlc
+import datetime
+import DateTime
 
-import pandas_datareader as web
-from datetime import datetime
 
 
 if __name__ == "__main__":
-    # # read from csv and plot
-    # current_path = os.path.dirname(os.path.abspath(__file__)) + '/'
-    # data_folder = current_path + "data/"
-    # db_folder = current_path + "database/"
-    # keys = ['Date', 'Days_Trade', 'Turnover_Value', 'Open', 'Day_High', 'Day_Low', 'Close', 'Price_Dif', 'Num_Deals']
-    # idx_col = ['Date']
-    #
-    # db_file = db_folder + "stockPrice.db"
-    # con = sqlite3.connect(db_file)
-    # for filename in os.listdir(os.path.join(data_folder)):
-    #     if os.path.splitext(filename)[1] != '.csv':
-    #         print('csv to sql: ignore file:' + filename)
-    #         continue
-    #     csv_file = data_folder + filename
-    #
-    #     df = pd.read_csv(csv_file, names=keys)
-    #     df.to_sql(name= filename[:-4], con=con, if_exists='replace')
-    #     #print(csv_file)
-    #     #print('csv to sql:' + filename[:-4])
-    #
-    # con.close()
 
-
+    keys = ['Date', 'Days_Trade', 'Turnover_Value', 'Open', 'Day_High', 'Day_Low', 'Close', 'Price_Dif', 'Num_Deals']
+    current_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+    data_folder = current_path + "data/"
+    db_folder = current_path + "database/"
     csv_file = data_folder + '2454.csv'
     csv_file2 = data_folder + '2330.csv'
     db_file = db_folder + "stockPrice.db"
     df = pd.read_csv(csv_file, names=keys)
     df2 = pd.read_csv(csv_file2, names=keys)
 
+    #
     con = sqlite3.connect(db_file)
     df.to_sql(name='2454', con=con, if_exists='replace')
     df2.to_sql(name='2330', con=con, if_exists='replace')
@@ -52,17 +36,62 @@ if __name__ == "__main__":
     # dfs1 = pd.read_sql_query("SELECT * from 2454 WHERE Date >='107/5/1'", con)
     # dfs1 = pd.read_sql_query("select * from '2454' ORDER BY 'index' DESC ", con=con)
     # dfs1 = pd.read_sql_query('select * from "2454" where "index" >= "4000" order by "index" ASC', con=con)
-    dfs1 = pd.read_sql_query('select * from "2454" order by "index" DESC LIMIT "10"', con=con)
+    dfs1 = pd.read_sql_query('select * from "2454" order by "index" DESC LIMIT "30"', con=con)
 
-    # for index, row in dfs1.iterrows():
-    #     tmp_file = row['Folder Path']
-    con.close()
-    print(dfs1)
-    print('Total items =' + str(len(dfs1)))
+    close_values = dfs1.Close.to_string(index=False)
+    print(close_values)
 
-    # plt.plot([1, 2, 3, 4])
-    # plt.ylabel('some numbers')
+    # plt.scatter(dfs1['Date'], dfs1['Close'])
     # plt.show()
+
+    ### candlestick plot
+    # data arrangement
+    # ohlc = []
+    # x = 0
+    # y = 10
+
+
+    # ohlc = [] # should be something like [(1, 2, 3, 4), (1, 2, 3, 4), (1, 2, 3, 4)] where 1 refer to the date
+    # a = 1, 2, 3, 4
+    # b = 5, 6, 7, 8
+    # ohlc.append(a)
+    # ohlc.append(b)
+    # print(ohlc)
+
+    # print(dfs1.columns[1:]) # get the column names
+
+    # if necessary convert to datetime
+    # print(dfs1['Date'].values)
+    for idx in range(len(dfs1)):
+        ori_date_format = dfs1['Date'].values[idx]
+        new_date_format = ori_date_format.replace(ori_date_format[0:3], str(int(ori_date_format[0:3]) + 1911))
+        new_date_format = datetime.datetime.strptime(new_date_format, '%Y/%m/%d').strftime('%Y-%m-%d')
+        dfs1['Date'].values[idx] = new_date_format
+
+    print(dfs1)
+
+    dfs1.Date = pd.to_datetime(dfs1.Date)
+    dfs1.Open = dfs1.Open.astype(float)
+    dfs1.Day_High = dfs1.Day_High.astype(float)
+    dfs1.Day_Low = dfs1.Day_Low.astype(float)
+    dfs1.Close = dfs1.Close.astype(float)
+    dfs1.Num_Deals = dfs1.Num_Deals.astype(float)
+
+    df_candleStickPlot = dfs1[['Date', 'Open', 'Day_High', 'Day_Low', 'Close', 'Num_Deals']]
+    df_candleStickPlot["Date"] = df_candleStickPlot["Date"].apply(mdates.date2num)
+
+    f1 = plt.subplot2grid((6, 4), (1, 0), rowspan=6, colspan=4, axisbg='#07000d')
+    candlestick_ohlc(f1, df_candleStickPlot.values, width=.6, colorup='#ff1717', colordown='#53c156')
+    f1.xaxis_date()
+    f1.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d %H:%M:%S'))
+
+    plt.xticks(rotation=45)
+    plt.ylabel('Stock Price')
+    plt.xlabel('Date Hours:Minutes')
+    plt.show()
+
+    #candlestick_ohlc(ax1, datas_candleStickPlot)
+
 
 
 # #plotly example (related to html, check it later)
