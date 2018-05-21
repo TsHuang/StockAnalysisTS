59352f639efddf1fe1d5e1a879d8bf459f70841b
TsHuang <tshuang1983@gmail.com>
2018-05-22 01:28:22 +0800

    Add Moving Average to the CandleStick plot

--- plot.py
+++ plot.py
@@ -13,82 +13,20 @@ import datetime
 import DateTime
 
 
-
-if __name__ == "__main__":
-
-    keys = ['Date', 'Days_Trade', 'Turnover_Value', 'Open', 'Day_High', 'Day_Low', 'Close', 'Price_Dif', 'Num_Deals']
-    current_path = os.path.dirname(os.path.abspath(__file__)) + '/'
-    data_folder = current_path + "data/"
-    db_folder = current_path + "database/"
-    csv_file = data_folder + '2454.csv'
-    csv_file2 = data_folder + '2330.csv'
-    db_file = db_folder + "stockPrice.db"
-    df = pd.read_csv(csv_file, names=keys)
-    df2 = pd.read_csv(csv_file2, names=keys)
-
-    #
-    con = sqlite3.connect(db_file)
-    df.to_sql(name='2454', con=con, if_exists='replace')
-    df2.to_sql(name='2330', con=con, if_exists='replace')
-    con.close()
-
-    # Read squlite query results into pandas DataFrame
-    con = sqlite3.connect(db_file)
-    # dfs1 = pd.read_sql_query("SELECT * from 2454 WHERE Date >='107/5/1'", con)
-    # dfs1 = pd.read_sql_query("select * from '2454' ORDER BY 'index' DESC ", con=con)
-    # dfs1 = pd.read_sql_query('select * from "2454" where "index" >= "4000" order by "index" ASC', con=con)
-    dfs1 = pd.read_sql_query('select * from "2454" order by "index" DESC LIMIT "30"', con=con)
-
-    close_values = dfs1.Close.to_string(index=False)
-    print(close_values)
-
-    # plt.scatter(dfs1['Date'], dfs1['Close'])
-    # plt.show()
-
-    ### candlestick plot
-    # data arrangement
-    # ohlc = []
-    # x = 0
-    # y = 10
-
-
-    # ohlc = [] # should be something like [(1, 2, 3, 4), (1, 2, 3, 4), (1, 2, 3, 4)] where 1 refer to the date
-    # a = 1, 2, 3, 4
-    # b = 5, 6, 7, 8
-    # ohlc.append(a)
-    # ohlc.append(b)
-    # print(ohlc)
-
-    # print(dfs1.columns[1:]) # get the column names
-
-    # if necessary convert to datetime
-    # print(dfs1['Date'].values)
-    for idx in range(len(dfs1)):
-        ori_date_format = dfs1['Date'].values[idx]
-        new_date_format = ori_date_format.replace(ori_date_format[0:3], str(int(ori_date_format[0:3]) + 1911))
-        new_date_format = datetime.datetime.strptime(new_date_format, '%Y/%m/%d').strftime('%Y-%m-%d')
-        dfs1['Date'].values[idx] = new_date_format
-
-    #print(dfs1)
-
-    dfs1.Date = pd.to_datetime(dfs1.Date)
-    dfs1.Open = dfs1.Open.astype(float)
-    dfs1.Day_High = dfs1.Day_High.astype(float)
-    dfs1.Day_Low = dfs1.Day_Low.astype(float)
-    dfs1.Close = dfs1.Close.astype(float)
-    dfs1.Num_Deals = dfs1.Num_Deals.astype(float)
-
+def candleStickPlot(dfs1, MAs):
     df_candleStickPlot = dfs1[['Date', 'Open', 'Day_High', 'Day_Low', 'Close', 'Num_Deals']]
     df_candleStickPlot['Date'] = df_candleStickPlot['Date'].apply(mdates.date2num)
-    df_candleStickPlot['MA50'] = df_candleStickPlot['Day_High'].rolling(10).mean()
-    print(df_candleStickPlot)
-    # df_candleStickPlot.reset_index(inplace=True)
 
-    #f1, ax = plt.subplot2grid((6, 4), (1, 0), rowspan=6, colspan=4, axisbg='#07000d')
     f1 = plt.subplot2grid((6, 4), (1, 0), rowspan=6, colspan=4, axisbg='#07000d')
-    #f1, ax = plt.subplots()
+
+    # plot candlestick
     candlestick_ohlc(f1, df_candleStickPlot.values, width=.6, colorup='#ff1717', colordown='#53c156')
-    f1.plot(df_candleStickPlot['Date'], df_candleStickPlot['MA50'])
+    # ohlc = [] # should be something like [(1, 2, 3, 4, 5), (1, 2, 3, 4, 5), (1, 2, 3, 4, 5)] where 1, 2, 3, 4 refer to the date, open, high, low, close, volumn
+
+    # plot MA
+    for MA_value in MAs:
+        ma_key = 'MA' + str(MA_value)
+        f1.plot(dfs1['Date'], dfs1[ma_key])  # plot MA
 
     f1.xaxis_date()
     # f1.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d %H:%M:%S'))
@@ -99,7 +37,56 @@ if __name__ == "__main__":
     plt.xlabel('Date')
     plt.show()
 
-    #candlestick_ohlc(ax1, datas_candleStickPlot)
+
+def getDataByStockIdx(stockIdx, MAs, days=30):
+    keys = ['Date', 'Days_Trade', 'Turnover_Value', 'Open', 'Day_High', 'Day_Low', 'Close', 'Price_Dif', 'Num_Deals']
+    current_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+    data_folder = current_path + "data/"
+    csv_file = data_folder + str(stockIdx) + '.csv'
+    df = pd.read_csv(csv_file, names=keys)
+
+    # Read squlite query results into pandas DataFrame
+    # con = sqlite3.connect(db_file)
+    # dfs1 = pd.read_sql_query('select * from "2454" order by "index" DESC LIMIT "30"', con=con)
+
+    MA_max = max(MAs)
+
+    df = df.tail(int(days) + int(MA_max) - 1)
+
+    # preprocessing
+    for idx in range(len(df)):
+        ori_date_format = df['Date'].values[idx]
+        new_date_format = ori_date_format.replace(ori_date_format[0:3], str(int(ori_date_format[0:3]) + 1911))
+        new_date_format = datetime.datetime.strptime(new_date_format, '%Y/%m/%d').strftime('%Y-%m-%d')
+        df['Date'].values[idx] = new_date_format
+
+    df.Date = pd.to_datetime(df.Date)
+    df.Open = df.Open.astype(float)
+    df.Day_High = df.Day_High.astype(float)
+    df.Day_Low = df.Day_Low.astype(float)
+    df.Close = df.Close.astype(float)
+    df.Num_Deals = df.Num_Deals.astype(float)
+
+    # df_candleStickPlot = df_candleStickPlot.iloc[::-1] #revert the data
+
+    # generate MA values
+    for MA_value in MAs:
+        new_key = 'MA' + str(MA_value)
+        df[new_key] = df['Close'].rolling(int(MA_value)).mean()
+
+    return df.tail(days)
+
+
+if __name__ == "__main__":
+    MAs = [5, 10, 20]  # Moving average of 5, 10, 20 days
+    dfs1 = getDataByStockIdx(stockIdx=2454, MAs=MAs, days=60)
+    candleStickPlot(dfs1=dfs1, MAs=MAs)
+
+
+    # print(dfs1.columns[1:]) # get the column names
+
+    # print(dfs1['Date'].values)
+
 
 
 
